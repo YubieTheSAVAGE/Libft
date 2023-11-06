c5fc34c5b533c7b7dcaae29776627214270409c5
aboudiba <aboudiba@c4r3p1.1337.ma>
2023-11-06 20:29:38 +0100

    125% [1st try]

---
+++ ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboudiba <aboudiba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 18:35:41 by aboudiba          #+#    #+#             */
+/*   Updated: 2023/11/02 22:50:12 by aboudiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*tmp;
+
+	if (!lst)
+		return (NULL);
+	tmp = lst;
+	while (tmp->next)
+		tmp = tmp->next;
+	return (tmp);
+}
